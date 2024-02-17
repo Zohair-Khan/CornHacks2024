8995bcb7df03c0b6e457fe619c8ae6f465497a76
Zohair-Khan <zohairkhanmail@gmail.com>
2024-02-17 15:48:14 -0600

    player/entity classes

--- game/classes/entity.py
+++ game/classes/entity.py
@@ -1,9 +1,11 @@
 from augments import augment
 class entity:
     augments = []
-    def __init__(self, name, hp, power, evasion, accuracy, critrate):
+    currenthp = 99999999
+    def __init__(self, name, maxhp, power, evasion, accuracy, critrate):
         self.name = name
-        self.hp = hp
+        self.maxhp = maxhp
+        self.currenthp = maxhp
         self.power = power
         self.evasion = evasion
         self.accuracy = accuracy
@@ -11,4 +13,26 @@ class entity:
     def addAugment(self, augment):
         self.augments.append(augment)
 
+    def getBaseStats(self):
+        return {"maxhp": self.hp,
+                "power": self.power,
+                "evasion": self.evasion,
+                "accuracy": self.accuracy,
+                "critrate": self.critrate}
 
+    def returnStats(self):
+        stats = self.getBaseStats()
+        for augment in self.augments:
+            if(augment.modifier == "multiply"):
+                stats[augment.attribute] = stats[augment.attr]*augment.value
+            elif (augment.modifier == "add"):
+                stats[augment.attribute] = stats[augment.attr]+augment.value
+        return stats
+
+    def modifyHP(self, value):
+        self.currenthp = self.currenthp+value
+        if(self.currenthp < 0):
+            self.currenthp = 0
+
+    def gameOver(self):
+        return (self.currenthp <= 0)
