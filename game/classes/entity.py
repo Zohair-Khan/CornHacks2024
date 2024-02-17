abbe1ed4d6996a47703eccb237cdeabc2624de77
swysocki9089 <160164665+swysocki9089@users.noreply.github.com>
2024-02-17 17:27:11 -0600

    Update entity.py

--- game/classes/entity.py
+++ game/classes/entity.py
@@ -1,9 +1,10 @@
 from augments import augment
 class entity:
     augments = [];
-    currenthp = 0;
+    currenthp = 999;
     baseStats = {};
     currentStats = {};
+
     def __init__(self, name, maxhp, power, evasion, accuracy, critrate):
         self.setName(name)
         self.setMaxHP(maxhp)
@@ -13,6 +14,7 @@ class entity:
         self.setCritRate(critrate)
         self.baseStats = self.getBaseStats();
         self.currentStats = dict(self.baseStats, currenthp = self.getMaxHP());
+
     def addAugment(self, augment):
         self.augments.append(augment)
 
@@ -27,46 +29,59 @@ class entity:
         stats = self.getBaseStats()
         for augment in self.augments:
             if(augment.modifier == "multiply"):
-                stats[augment.attribute] = stats[augment.attr]*augment.value
+                stats[augment.attribute] = stats[augment.attribute]*augment.value
             elif (augment.modifier == "add"):
-                stats[augment.attribute] = stats[augment.attr]+augment.value
-        self.currentStats = stats;
+                stats[augment.attribute] = stats[augment.attribute]+augment.value
+        self.currentStats.update(stats);
 
     def returnStats(self):
-        self.updateStats;
+        self.updateStats();
         return self.currentStats;
 
     def modifyHP(self, value):
         maxhp = self.returnStats()["maxhp"]
-        self.currentStats["currenthp"] = self.currentStats["currenthp"]+value
-        if(self.currentStats["currenthp"] < 0):
+        self.setCurrentHP(self.getCurrentHP()+value)
+        if(self.getCurrentHP() <= 0):
             self.gameOver();
-        if(self.currentStats["currenthp"] > maxhp):
-            self.currentStats["currenthp"] = maxhp
+        if(self.getCurrentHP > maxhp):
+            self.setCurrentHP(maxhp)
 
     def gameOver(self):
-        return (self.currenthp <= 0)
+        return (self.getCurrentHP() <= 0)
 
+
+
+    #Getters and Setters
     def setName(self, name):
         self.name = name;
     def getName(self):
         return self.name;
+
     def setMaxHP(self, maxHP):
         self.maxhp = maxHP;
     def getMaxHP(self):
         return self.maxhp;
+
+    def setCurrentHP(self, currenthp):
+        self.currentStats["currenthp"] = currenthp;
+    def getCurrentHP(self):
+        return self.currentStats["currenthp"]
+
     def setPower(self, power):
         self.power = power;
     def getPower(self):
         return self.power;
+
     def setEvasion(self, evasion):
         self.evasion = evasion;
     def getEvasion(self):
-        return self.evasion;       
+        return self.evasion;    
+
     def setAccuracy(self, accuracy):
         self.accuracy = accuracy;
     def getAccuracy(self):
         return self.accuracy; 
+
     def setCritRate(self, critrate):
         self.critrate = critrate;
     def getCritRate(self):
