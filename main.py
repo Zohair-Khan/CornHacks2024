5653b91731669884116bf17c6889d40f114e5cb7
joeyngg <111897766+joeyngg@users.noreply.github.com>
2024-02-17 14:36:25 -0600

    Added background

--- main.py
+++ main.py
@@ -9,13 +9,26 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("EVEN Steven Beats ODDS")
 
+bg_image = pygame.image.load("assets/background/battleground.jpg").convert_alpha()
+
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
 # game loop
 run = True
 while run:
+
+    # draw background
+    draw_bg()
 
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # update display
+    pygame.display.update()
+
 # exit pygame
 pygame.quit()
