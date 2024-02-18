5661b45738ab24c9ae6b01049efbe3ca37b3372d
swysocki9089 <swysocki2@huskers.unl.edu>
2024-02-18 07:15:43 -0600

    Added comments, cleaned up stat update

--- game/classes/entity.py
+++ game/classes/entity.py
@@ -1,12 +1,14 @@
 from augments import augment
 class entity:
     def __init__(self, name, maxhp, power, evasion, accuracy, critrate):
+        # set all attributes
         self.setName(name)
         self.setMaxHP(maxhp)
         self.setPower(power)
         self.setEvasion(evasion)
         self.setAccuracy(accuracy)
         self.setCritRate(critrate)
+        # initialize stats dictionaries and augments list
         self.baseStats = self.getBaseStats();
         self.currentStats = dict(self.baseStats, currenthp = self.getMaxHP());
         self.augments = [];
@@ -17,6 +19,7 @@ class entity:
         self.updateStats()
 
     def getAugments(self):
+        # Print out a list of augment names
         str = ""
         for augment in self.augments:
             str+=augment.name;
@@ -35,29 +38,25 @@ class entity:
         stats.update({"currenthp": self.getCurrentHP()});
         for augment in self.augments:
             if(augment.modifier == "multiply"):
-                if(augment.attribute == "maxhp"):
-                    stats["maxhp"] *= augment.value;
-                    # self.modifyHP(stats["currenthp"]*(augment.value-1))
-                else:
                     stats[augment.attribute]*=augment.value
             elif(augment.modifier == "add"):
-                if(augment.attribute == "maxhp"):
-                    stats["maxhp"] += augment.value;
-                    # self.modifyHP(augment.value)
-                else:
                     stats[augment.attribute]+=augment.value
-            if(augment.attribute == "maxhp" and stats["currenthp"] > stats["maxhp"]):
-                stats["currenthp"] = stats["maxhp"]
+            if(augment.attribute == "maxhp"):
+                # adjusts currenthp with augments without exceeding maxhp
+                stats["currenthp"] = min(stats["maxhp"],stats["currenthp"]*augment.value if augment.modifier == "multiply" else stats["currenthp"]+augment.value)
         self.currentStats.update(stats);
 
     def returnStats(self):
+        #updates stats and returns current stats
         self.updateStats();
         return self.currentStats;
 
     def modifyHP(self, value):
+        # ensure modified hp does not exceed maxhp and triggers game over event if health drops to 0 or less
         maxhp = self.returnStats()["maxhp"]
         self.setCurrentHP(self.getCurrentHP()+value)
         if(self.getCurrentHP() <= 0):
+            self.setCurrentHP(0);
             self.gameOver();
         if(self.getCurrentHP() > maxhp):
             self.setCurrentHP(maxhp)
@@ -66,6 +65,7 @@ class entity:
         self.alive = False;
 
     def dispStats(self):
+        # Debug view of stats
         print(f"Name: {self.name}")
         print(f"HP: {self.getCurrentHP()}/"+str(self.currentStats["maxhp"]));
         print(f"Power: "+ str(self.currentStats["power"]))
