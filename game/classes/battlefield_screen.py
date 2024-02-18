52fcd0fc111b5a03c12e09e4b85180915b92f9a5
joeyngg <111897766+joeyngg@users.noreply.github.com>
2024-02-17 23:20:57 -0600

    attack with key press

--- game/classes/battlefield_screen.py
+++ game/classes/battlefield_screen.py
@@ -10,6 +10,9 @@ def battlefield_screen(screen, SCREEN_WIDTH, SCREEN_HEIGHT, player):
     BLACK = (0, 0, 0)
     GRAY = (192, 192, 192)
     LIGHT_GREEN = (144, 238, 144)
+    RED = (255, 0, 0)
+    YELLOW = (255, 255, 0)
+    WHITE = (255, 255, 255)
 
     # Load background image or draw the battlefield background
     background_image = pygame.image.load(
@@ -17,24 +20,40 @@ def battlefield_screen(screen, SCREEN_WIDTH, SCREEN_HEIGHT, player):
     background_image = pygame.transform.scale(
         background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # health bar
+    def draw_health_bar(health, x, y):
+        ratio = health / 100
+        pygame.draw.rect(screen, WHITE, (x-2, y-2, 404, 34))
+        pygame.draw.rect(screen, RED, (x, y, 400, 30))
+        pygame.draw.rect(screen, YELLOW, (x, y, 400*ratio, 30))
+
     # load player image
     player_image = pygame.image.load("assets/characterImages/Steven.png")
     player_image = pygame.transform.scale(player_image, (400, 400))
 
     # create player
     player_1 = player("Steven", 100, 100, 100, 100, 100)
-    fighter_1 = Fighter(100, 100)
+    fighter_1 = Fighter(100, 200)
+    fighter_2 = Fighter(700, 200)
 
     # Game loop for the battlefield screen
     while True:
         screen.fill(WHITE)
         screen.blit(background_image, (0, 0))
+
+        # show health bar
+        draw_health_bar(fighter_1.health, 20, 20)
+        draw_health_bar(fighter_2.health, 580, 20)
+
         # Draw the player on the screen
         screen.blit(player_image, (60, 100))
         player_1.move()
 
+        # move fighter
+        fighter_1.move(SCREEN_WIDTH, screen, fighter_2)
+        # draw fighter
         fighter_1.draw(screen)
-        fighter_1.move()
+        fighter_2.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- game/classes/fighter.py
+++ game/classes/fighter.py
@@ -3,9 +3,12 @@ import pygame
 
 class Fighter():
     def __init__(self, x, y):
-        self.rect = pygame.Rect((x, y, 100, 180))
+        self.rect = pygame.Rect((x, y, 100, 280))
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
 
-    def move(self):
+    def move(self, screen_width, surface, target):
         SPEED = 1
         dx = 0
         dy = 0
@@ -13,15 +16,37 @@ class Fighter():
         # get keypress
         key = pygame.key.get_pressed()
 
-        # movememt
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
+        # can only perform move when not currently attacking
+        if self.attacking == False:
+            # movememt
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
 
-        # update player position
-        self.rect.x += dx
-        self.rect.y += dy
+            # attack
+            if key[pygame.K_r]:
+                self.attack(surface, target)
+                self.attacl_type = 1
+
+            # ensure player stays on screen
+            if self.rect.left + dx < 0:
+                dx = -self.rect.left
+            if self.rect.right + dx > screen_width:
+                dx = screen_width - self.rect.right
+
+            # update player position
+            self.rect.x += dx
+            self.rect.y += dy
+
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(
+            self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+
+        pygame.draw.rect(surface, (255, 0, 0), attacking_rect)
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        pygame.draw.rect(surface, (0, 255, 0), self.rect)
