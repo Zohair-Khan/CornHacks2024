d90ba0f2ab66cc838f189c60726296c46a16354d
joeyngg <111897766+joeyngg@users.noreply.github.com>
2024-02-17 21:01:29 -0600

    trying to make player move

--- game/classes/fight.py
+++ game/classes/fight.py
@@ -2,15 +2,18 @@ from player import player
 from enemy import enemy
 from entity import entity
 import random
+
+
 class fight:
     def attack(attacker: entity, defender: entity):
         attackerstats = attacker.returnStats()
-        
-        attackodds = attackerstats["accuracy"]*(1.0-defender.returnStats()["evasion"])
-        if(random.random() > attackodds):
+
+        attackodds = attackerstats["accuracy"] * \
+            (1.0-defender.returnStats()["evasion"])
+        if (random.random() > attackodds):
             return
         else:
             damage = -1.0*attackerstats["power"]
-            if(random.random() <= attackerstats["critrate"]):
+            if (random.random() <= attackerstats["critrate"]):
                 damage = damage*1.5
             defender.modifyHP(damage)
--- game/classes/sampleEntityTests.py
+++ game/classes/sampleEntityTests.py
@@ -5,29 +5,33 @@ from fight import fight
 from augments import augment
 from enemy import enemy
 
+
 class TestEntityMethods(unittest.TestCase):
 
     def test_init_player_currenthp(self):
-        character = player("Even Steven",20,10,.3,.6,.15);
-        stats = character.returnStats();
-        self.assertEqual(stats["maxhp"], 20);
+        character = player("Even Steven", 20, 10, .3, .6, .15)
+        stats = character.returnStats()
+        self.assertEqual(stats["maxhp"], 20)
         self.assertEqual(stats["currenthp"], character.getMaxHP())
 
     def test_power_adding_augment(self):
-        character = player("Even Steven",20,10,.3,.6,.15);
-        powerAddingAugment = augment("Lesser Power Augment of Addition", "power", "add", 5);
-        character.addAugment(powerAddingAugment);
+        character = player("Even Steven", 20, 10, .3, .6, .15)
+        powerAddingAugment = augment(
+            "Lesser Power Augment of Addition", "power", "add", 5)
+        character.addAugment(powerAddingAugment)
         # print(character.augments);
-        stats = character.returnStats();
-        self.assertEqual(stats["power"],15);
+        stats = character.returnStats()
+        self.assertEqual(stats["power"], 15)
 
     def test_power_multiplying_augment(self):
-        character = enemy("Seven of Heaven",20,10,.3,.6,.15);
-        powerMultiplyingAugment = augment("Lesser Power Augment of Multiplication", "power", "multiply", 1.2);
-        character.addAugment(powerMultiplyingAugment);
+        character = enemy("Seven of Heaven", 20, 10, .3, .6, .15)
+        powerMultiplyingAugment = augment(
+            "Lesser Power Augment of Multiplication", "power", "multiply", 1.2)
+        character.addAugment(powerMultiplyingAugment)
         # print(character.augments);
-        stats = character.returnStats();
-        self.assertEqual(stats["power"],12);
+        stats = character.returnStats()
+        self.assertEqual(stats["power"], 12)
+
 
 if __name__ == '__main__':
     unittest.main()
